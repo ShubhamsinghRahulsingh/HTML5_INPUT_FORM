d2712e106a59df60b756734d166ca3df4d5cc26d
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-31 18:25:36 +0530

    (Shubham)Add: Validating Email using Regex

--- InputForm/JS/inptForm.js
+++ InputForm/JS/inptForm.js
@@ -1,12 +1,23 @@
 //UC1 validate name
-        const text=document.querySelector('#text');
-        const textError=document.querySelector('.text-error');
-        text.addEventListener('input',function(){
-            let nameRegex=RegExp('^[A-Z]{1}[a-z]{3,}$');
-            if(nameRegex.test(text.value))
-            textError.textContent="";
-            else
-            textError.textContent="Invalid Name";
-        });
+const text = document.querySelector('#text');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input', function () {
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+    if (nameRegex.test(text.value))
+        textError.textContent = "";
+    else
+        textError.textContent = "Invalid Name";
+});
 
-       
+//UC2 Email validation
+const email = document.querySelector("#Email");
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input', function () {
+    let emailRegex = RegExp('^[a-z]+([.+-_][a-z]+)*@[a-z]+.[a-z]+([.]+[a-z]+)*$');
+    if (emailRegex.test(Email.value)) {
+        emailError.textContent = ""
+    }
+    else {
+        emailError.textContent = "Email is not valid";
+    }
+});
